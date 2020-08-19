9eaa3935a2613de9103442a1980021c5c6b1c536
licheng <licheng@wdit.com.cn>
2020-08-19 10:39:21 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,170 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="quill-box" :style="{ height: fullheight + 'px'}">
+    <a-modal v-model="visible" title="秀米" width="90%" :footer="null" :maskClosable="false" :centered="true" :keyboard="false">
+      <div v-if="visible">
+        <iframe src="./pluging/xiumi-ue-dialog-v5.html" frameborder="0" width="100%" :height="(fullheight - 150)+'px'" id="xiumiIframe"></iframe>
+      </div>
+    </a-modal>
+    <a-modal v-model="visible2" title="135编辑器" width="90%" :footer="null" :maskClosable="false" :centered="true" :keyboard="false">
+      <div v-if="visible2">
+        <iframe src="./pluging/135EditorDialogPage.html" frameborder="0" width="100%" :height="(fullheight - 150)+'px'" id="xiumiIframe"></iframe>
+      </div>
+    </a-modal>
+    <div class="quill-editor">
+      <div id="toolbar" slot="toolbar">
+        <!-- Add a bold button -->
+        <button class="ql-bold" title="加粗"></button>
+        <!-- Add a 秀米 button -->
+        <button id="custom-button-xiumi" title="秀米" @click="showModal"></button>
+        <!-- Add a 135 button -->
+        <button id="custom-button-135" title="135编辑器" @click="showModal2"></button>
+      </div>
+      <div class="editor" ref="editor"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // 引入原始组件
+  import * as Quill from 'quill'
+  // 引入核心样式和主题样式
+  import 'quill/dist/quill.core.css'
+  import 'quill/dist/quill.snow.css'
+  // 引入自定义blot
+  import blotSelect from './components/blot'
+  blotSelect(Quill)
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'quill-xiumi-135',
+  props: {
+    // 用于双向绑定
+    content: String,
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      visible: false,
+      visible2: false,
+      selection: {},
+      fullheight: document.documentElement.clientHeight,
+      // 待初始化的编辑器
+      quill: null,
+      options: {
+          theme: 'snow',
+          modules: {
+            // 工具栏的具体配置
+            toolbar: "#toolbar",
+          },
+          placeholder: '请输入内容...'
+        }
+    }
+  },
+  mounted () {
+    // 初始化编辑器
+    this._initEditor()
+    window.setRichText = this.setRichText
+  },
+  methods:{
+    // 初始化编辑器
+    _initEditor () {
+      // 获取编辑器的DOM容器
+      let editorDom = this.$el.querySelector('.editor')
+      // 初始化编辑器
+      this.quill = new Quill(editorDom, this.options)
+      // select
+      this.quill.on('selection-change', range => {
+        this.selection = this.quill.getSelection()
+        if (!range) {
+          this.$emit('blur', this.quill)
+        } else {
+          this.$emit('focus', this.quill)
+        }
+      })
+      // 双向绑定
+      this.quill.on('text-change', () => {
+          this.emitChange()
+          this.selection = this.quill.getSelection()
+        })
+      // 插入内容
+      this.firstSetHtml()
+      this.$emit('ready', this.quill)
+    },
+    firstSetHtml() {
+      let nodeHtml = ''
+      let msg = ''
+      if(this.value) {
+        // 判断是否有秀米和或135元素
+        if(this.value.indexOf('xiumi.us') > -1 || this.value.indexOf('135editor.com') > -1 ) {
+          // 已经有包裹元素，则去掉
+          if(this.value.indexOf("rich-innerHtml") > -1) {
+            nodeHtml =  new DOMParser().parseFromString(this.value,'text/html').body.childNodes[0]
+            msg = nodeHtml.firstChild.innerHTML
+          }
+          // 通过新blot，插入代码
+          this.setRichText(msg || this.value)
+        } else {
+          // 正常插入
+          this.quill.clipboard.dangerouslyPasteHTML(this.value)
+          // this.$refs.editor.children[0].innerHTML = this.value
+        }
+      }
+    },
+    // 更新text-change
+    emitChange() {
+      let html = this.$refs.editor.children[0].innerHTML
+      const quill = this.quill
+      const text = this.quill.getText()
+      if (html === '<p><br></p>') html = ''
+      this.$emit('input', html)
+      this.$emit('change', { html, text, quill })
+      this.$emit("getConetntLength", this.quill.getLength())
+    },
+    setRichText(e) {
+      const index = this.selection?this.selection.index: 0
+      this.quill.insertEmbed(index ||  0, 'AppPanelEmbed', e)
+      this.visible = false
+      this.visible2 = false
+    },
+    showModal() {
+      this.visible = true
+    },
+    showModal2() {
+      this.visible2 = true
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.quill-editor {
+  height: 60%;
+}
+#toolbar button {
+  outline:none
+}
+#custom-button-xiumi, #custom-button-135 {
+  background-size: contain;
+  background-repeat: no-repeat;
+  height: 16px;
+  margin-top: 4px;
+  width: 33px;
+  padding-right: 8px;
+  background-position: center;
+  position: relative;
+}
+#custom-button-xiumi {
+  background-image: url('http://xiumi.us/connect/ue/xiumi-connect-icon.png');
+}
+#custom-button-xiumi:hover {
+  background-image: url('http://xiumi.us/connect/ue/xiumi-connect-icon.png');
+}
+#custom-button-135 {
+  background-image: url('http://static.135editor.com/img/icons/editor-135-icon.png');
+}
+#custom-button-135:hover {
+  background-image: url('http://static.135editor.com/img/icons/editor-135-icon.png');
 }
 </style>
---
+++ src/components/blot.js
@@ -0,0 +1,34 @@
+export default function (Quill) {
+  // 引入源码中的BlockEmbed
+  const BlockEmbed = Quill.import('blots/block/embed');
+  // 定义新的blot类型
+  class AppPanelEmbed extends BlockEmbed {
+    static create(value) {
+      const node = super.create(value);
+      // node.setAttribute('contenteditable', 'false');
+      // node.setAttribute('width', '100%');
+      //   设置自定义html
+      node.innerHTML = this.transformValue(value)
+      return node;
+    }
+
+    static transformValue(value) {
+      let handleArr = value.split('\n')
+      handleArr = handleArr.map(e => e.replace(/^[\s]+/, '')
+        .replace(/[\s]+$/, ''))
+      return handleArr.join('')
+    }
+
+    // 返回节点自身的value值 用于撤销操作
+    static value(node) {
+      return node.innerHTML
+    }
+  }
+  // blotName
+  AppPanelEmbed.blotName = 'AppPanelEmbed';
+  // class名将用于匹配blot名称
+  AppPanelEmbed.className = 'rich-innerHtml';
+  // 标签类型自定义
+  AppPanelEmbed.tagName = 'div';
+  Quill.register(AppPanelEmbed, true);
+}
