61433c1398a62bd7730576789b3093d293c7b161
flex <814114037@qq.com>
2021-03-08 23:35:57 +0800

    remove:text.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <quillEditor v-model="msg"/>
+    <quillEditor v-model="msg" class="my-editor"/>
   </div>
 </template>
 
@@ -8,15 +8,22 @@
 import quillEditor from './components/quill';
 export default {
   name: 'quill-xiumi-135',
-  components: {
-    quillEditor
-  },
+  components: {quillEditor},
   data() {
     return {
-      msg: ''
+      msg: '111'
     }
   },
   methods:{
   }
 }
 </script>
+<style>
+/* .my-editor,.my-text {
+  width: 50%;
+  display: inline-block;
+}
+.my-text {
+  vertical-align: top;
+} */
+</style>
